ce4032809b0cbc596e4ff80155772b1533d0a3d0
[Github <lg981222@naver.com>
2022-07-28 09:50:59 +0900

    2022/07/28

--- src/ja_0728/ArrayList_3.java
+++ src/ja_0728/ArrayList_3.java
@@ -15,10 +15,12 @@ public class ArrayList_3 {
 	numbers.add("40");
 	System.out.println("add(값)");
 	System.out.println(numbers);
+	//arrylist 값 출력
 	
 	numbers.add(1, "50");
 	System.out.println("\nadd(인덱스, 값)");
 	System.out.println(numbers);
+	//2번째에 50 집어넣기
 	
 	numbers.remove(2);
 	System.out.println("\nremove(인덱스)");
@@ -40,11 +42,17 @@ public class ArrayList_3 {
 		String value = (String)it.next();
 		if(value.equals("30")) {
 			it.remove();
-			
+	//30 발견하면 지워버리기	
 		}
 		System.out.println(value);
 	}
 	System.out.println(numbers);
+	
+	System.out.println("\nfor each");
+	for(String value : numbers) {
+		System.out.println(value);
+	}
+	//foreach 이게 향상된 for문 근데 결과는 같음 ㅋㅋ
 	
 	System.out.println("\nfor");
 	for(int i = 0; i< numbers.size(); i++) {
